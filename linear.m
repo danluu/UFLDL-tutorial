f8a74bf826688fec62cb6db7ef9785b8a368c166
Dan Luu <danluu@google.com>
2013-08-30 13:34:34 -0500

    Plot J vals

--- linear.m
+++ linear.m
@@ -18,7 +18,6 @@ while abs(max(delta(:))) > 0.00001
     h = sum(x * theta,2);
     err = h - y;
     delta = x' * err / m;
-    delta
     theta = theta - alpha * delta;
 end
 
@@ -29,4 +28,26 @@ plot(x(:,2), x*theta, '-') % remember that x is now a matrix with 2
                            % time info
 legend('Training data', 'Linear regression')
 
+J_vals = zeros(100, 100);   % initialize Jvals to 100x100 matrix of
+                            % 0's
+                            
+
+theta0_vals = linspace(-3, 3, 100);
+theta1_vals = linspace(-1, 1, 100);
+for i = 1:length(theta0_vals)
+      for j = 1:length(theta1_vals)
+            t = [theta0_vals(i); theta1_vals(j)];
+            h = sum(x * t);
+            J_vals(i,j) = sum((h - y).^2);
+    end
+end
+
+% Plot the surface plot
+% Because of the way meshgrids work in the surf command, we need to 
+% transpose J_vals before calling surf, or else the axes will be
+% flipped
+J_vals = J_vals';
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('\theta_0'); ylabel('\theta_1')
 
