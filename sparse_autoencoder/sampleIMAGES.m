8e215a97b44e0dc54cbb90af50332598c290b156
Dan Luu <danluu@google.com>
2013-09-12 18:59:41 -0500

    Regularization and sparsity. Sparsity is wrong

--- sparse_autoencoder/sampleIMAGES.m
+++ sparse_autoencoder/sampleIMAGES.m
@@ -5,7 +5,7 @@ function patches = sampleIMAGES()
 load IMAGES;    % load images from disk 
 
 patchsize = 8;  % we'll use 8x8 patches 
-                %numpatches = 10000;
+numpatches = 10000;
 numpatches = 10;
 
 % Initialize patches with zeros.  Your code will fill in this matrix--one
--- sparse_autoencoder/train.m
+++ sparse_autoencoder/train.m
@@ -23,11 +23,8 @@ lambda = 0.0001;     % weight decay parameter
 beta = 3;            % weight of sparsity penalty term       
 
 % FIXME: changing this while we test partial implementation
-lambda = 0; 
-hiddenSize = 5;
+lambda = 0.0001; 
 beta = 0;
-%beta = 0;      
-%hiddenSize = 3; 
 
 %%======================================================================
 %% STEP 1: Implement sampleIMAGES
@@ -86,17 +83,17 @@ checkNumericalGradient();
 
 % Now we can use it to check your cost function and derivative calculations
 % for the sparse autoencoder.  
-numgrad = computeNumericalGradient( @(x) sparseAutoencoderCost(x, visibleSize, ...
+ numgrad = computeNumericalGradient( @(x) sparseAutoencoderCost(x, visibleSize, ...
                                                   hiddenSize, lambda, ...
                                                   sparsityParam, beta, ...
                                                   patches), theta);
 
 % Use this to visually compare the gradients side by side
-disp([numgrad grad]); 
+ disp([numgrad grad]); 
 
 % Compare numerically computed gradients with the ones obtained from backpropagation
-diff = norm(numgrad-grad)/norm(numgrad+grad);
-disp(diff); % Should be small. In our implementation, these values are
+ diff = norm(numgrad-grad)/norm(numgrad+grad);
+ disp(diff); % Should be small. In our implementation, these values are
             % usually less than 1e-9.
 
             % When you got this working, Congratulations!!! 
