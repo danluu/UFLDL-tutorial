3fe7db3d73a368f786001b247ed27894f0a4ad88
Dan Luu <danluu@google.com>
2013-09-26 13:26:59 -0500

    Cache results between runs

--- self_taught_learning/stlExercise.m
+++ self_taught_learning/stlExercise.m
@@ -71,13 +71,26 @@ theta = initializeParameters(hiddenSize, inputSize);
 
 opttheta = theta; 
 
+addpath './minFunc/'
+options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
+                          % function. Generally, for minFunc to work, you
+                          % need a function pointer with two outputs: the
+                          % function value and the gradient. In our problem,
+                          % sparseAutoencoderCost.m satisfies this.
+options.maxIter = 400;	  % Maximum number of iterations of L-BFGS to run 
+options.display = 'on';
 
 
+% [opttheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
+%                                   visibleSize, hiddenSize, ...
+%                                   lambda, sparsityParam, ...
+%                                   beta, patches), ...
+%                              theta, options);
 
 
+% save('min_func_result.mat', 'trainSet', 'testSet', 'unlabeledData', 'trainData', 'trainLabels', 'testData', 'testLabels', 'opttheta');
 
-
-
+load('min_func_result.mat');
 
 %% -----------------------------------------------------
                           
